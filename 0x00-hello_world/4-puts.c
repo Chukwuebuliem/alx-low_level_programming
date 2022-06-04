14ac5168c6cb1e94f55b20a59a659786a880088f
Chukwuebuliem <chukwuebuliemsuccess@gmail.com>
2022-06-04 09:06:45 -0700

    Programming testing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print a line of string in the standard output
+*
+* Description: this will print a line of string describing what programming is
+*
+* Return: 0 is successful
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
