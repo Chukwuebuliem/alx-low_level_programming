1ba78bbc989d33c323d168141e16c5d7f23d773a
Chukwuebuliem <chukwuebuliemsuccess@gmail.com>
2022-06-04 09:25:43 -0700

    using printf to print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - use printf to print a line of string in the standard output
+*
+* Description: this will print a line of string using printf
+*
+* Return: 0 is true
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
