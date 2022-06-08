34ad96fdbc74d6e782f5d480823d12bd62bdc8f2
Chukwuebuliem <chukwuebuliemsuccess@gmail.com>
2022-06-08 13:33:07 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  *void print_alphabet_x10(void) - print alphabet x10
+ *   *
+ *    *Return: Always 0.
+ */
+print_alphabet_x10(void)
+{
+	int c;
+
+	for (c = '0'; c < '10'; c++)
+	{
+		int d;
+
+		for (d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+	}
+	return (0);
+}
