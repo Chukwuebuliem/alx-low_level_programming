b35de08a44b62f073d3202155275176392029a43
Chukwuebuliem <chukwuebuliemsuccess@gmail.com>
2022-06-08 12:00:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - print the file putchar
+* main - a file with the name 0-putchar.c
 *
-* Description: this will print the file 0-putchar.c
+* Description: this will print the word _putchar, then a new line
 *
 * Return: 0 is successful
 */
 int main(void)
 {
-	puts("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
